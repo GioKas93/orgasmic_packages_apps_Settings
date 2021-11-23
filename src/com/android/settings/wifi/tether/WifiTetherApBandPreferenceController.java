IPSBHANGU/packages_apps_Settings

WiFi: Remove country code checking.
Always allow 5gHz AP option

Change-Id: I1205c7cec7715c1fac25eb29a311e347855f4d49

src/com/android/settings/wifi/tether/WifiTetherApBandPreferenceController.java
@@ -97,11 +97,7 @@ public class WifiTetherApBandPreferenceController extends WifiTetherBasePreferen
     }
 
     private boolean is5GhzBandSupported() {
-        final String countryCode = mWifiManager.getCountryCode();
-        if (!mWifiManager.isDualBandSupported() || countryCode == null) {
-            return false;
-        }
-        return true;
+        return mWifiManager.isDualBandSupported();
     }
 
     public int getBandIndex() {

GioKas93/orgasmic_packages_apps_Settings

src/com/android/settings/wifi/tether/WifiTetherApBandPreferenceController.java
@@ -126,11 +126,7 @@ public class WifiTetherApBandPreferenceController extends WifiTetherBasePreferen
     }
 
     private boolean is5GhzBandSupported() {
-        final String countryCode = mWifiManager.getCountryCode();
-        if (!mWifiManager.is5GHzBandSupported() || countryCode == null) {
-            return false;
-        }
-        return true;
+        return mWifiManager.is5GHzBandSupported();
     }
 
     public int getBandIndex() {